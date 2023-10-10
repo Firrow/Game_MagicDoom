f8dae96dc3dc7cbccc048ab6d2066fe89d3ca0f4
Firrow <firrowmb@gmail.com>
2023-10-10 18:00:12 +0200

    Spawn Enemy
    Only two zones of spawn

---
+++ MagicDoom/Assets/Scripts/ZoneSpawn.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZoneSpawn : MonoBehaviour
+{
+    public GameObject enemy;
+
+    private Vector2 spawnPosition;
+    private float delay = 1f; //à voir si temps aléatoire ou évolu
+
+    void Start()
+    {
+        StartCoroutine(Spawn());
+    }
+
+    void Update()
+    {
+
+    }
+
+    IEnumerator Spawn()
+    {
+        spawnPosition = new Vector2(Random.Range(this.transform.position.x-0.3f, this.transform.position.x+0.3f), Random.Range(this.transform.position.y-2f, this.transform.position.y + 2f));
+        if(this.gameObject.name == "ZoneSpawn1")
+            Instantiate(enemy, spawnPosition, Quaternion.Euler(0, 0, 0));
+        else
+            Instantiate(enemy, spawnPosition, Quaternion.Euler(0, -180, 0));
+        yield return new WaitForSeconds(delay);
+
+        StartCoroutine(Spawn());
+    }
+}
+
