a684cea7503f3a604606758e7caa2ce714165222
Firrow <firrowmb@gmail.com>
2023-10-15 17:29:10 +0200

    Update script SpawnZone

--- MagicDoom/Assets/Scripts/Enemy.cs
+++ MagicDoom/Assets/Scripts/Enemy.cs
@@ -12,23 +12,39 @@ public class Enemy : MonoBehaviour
     void Start()
     {
         speed = 0.5f;
-        maxDistance = 1000;
+        maxDistance = float.PositiveInfinity;
 
         foreach (var item in GameObject.FindGameObjectsWithTag("Cauldron"))
         {
             potentialTarget.Add(item);
         }
-        potentialTarget.Add(GameObject.FindGameObjectWithTag("Player"));
+        foreach (var item in GameObject.FindGameObjectsWithTag("Player"))
+        {
+            potentialTarget.Add(item);
+        }
+
 
         ChoiceTarget();
+        EnemyOrientation();
     }
 
 
     void Update()
     {
+        ChoiceTarget();
         //when enemy is close to target
         if (Vector2.Distance(transform.position, targetEnemy.transform.position) > 0.5f)
             transform.position = Vector2.MoveTowards(transform.position, targetEnemy.transform.position, speed * Time.deltaTime);
+    }
+
+
+
+    public void EnemyOrientation()
+    {
+        if (this.transform.position.x > GameObject.FindGameObjectWithTag("Player").transform.position.x)
+            this.transform.rotation = Quaternion.Euler(0, 0, 0);
+        else
+            this.transform.rotation = Quaternion.Euler(0, -180, 0);
     }
 
     private void ChoiceTarget()
@@ -37,8 +53,10 @@ public class Enemy : MonoBehaviour
         {
             var distance = Vector3.Distance(transform.position, item.transform.position);
             if (distance < maxDistance)
+            {
                 targetEnemy = item;
                 maxDistance = distance;
+            }
         }
     }
 }
