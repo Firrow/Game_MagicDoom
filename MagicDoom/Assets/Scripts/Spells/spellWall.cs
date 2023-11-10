7716964f00f93f48e342286315ab566d53c647ef
Firrow <firrowmb@gmail.com>
2023-11-10 23:08:51 +0100

    Recalculate Wall's Collider Shape during destruction

--- MagicDoom/Assets/Scripts/Spells/spellWall.cs
+++ MagicDoom/Assets/Scripts/Spells/spellWall.cs
@@ -9,15 +9,17 @@ public class spellWall : MonoBehaviour
     private int defaultHealth;
     private List<GameObject> enemiesImCollidingWith = new List<GameObject>();
     private SpriteRenderer spriteRenderer;
+    private PolygonCollider2D collider;
 
     [SerializeField] Sprite[] wallSprites;
 
 
     void Start()
     {
-        defaultHealth = 30;
+        defaultHealth = 50;
         Health = defaultHealth;
         spriteRenderer = this.GetComponent<SpriteRenderer>();
+        collider = this.GetComponent<PolygonCollider2D>();
     }
 
 
@@ -58,19 +60,33 @@ public class spellWall : MonoBehaviour
             Health -= enemy.GetComponent<Enemy>().Damage;
         }
 
+
         if (Health > defaultHealth * 0.67)
+        {
             spriteRenderer.sprite = wallSprites[0];
+            RecalculateColliderShape();
+        }
         else if (Health > defaultHealth * 0.33)
+        {
             spriteRenderer.sprite = wallSprites[1];
+            RecalculateColliderShape();
+        }
         else if (Health > 0)
+        {
             spriteRenderer.sprite = wallSprites[2];
+            RecalculateColliderShape();
+        }
         else
         {
             Destroy(this.gameObject);
         }
     }
 
-
+    private void RecalculateColliderShape()
+    {
+        Destroy(GetComponent<PolygonCollider2D>());
+        gameObject.AddComponent<PolygonCollider2D>();
+    }
 
     public int Health
     {
