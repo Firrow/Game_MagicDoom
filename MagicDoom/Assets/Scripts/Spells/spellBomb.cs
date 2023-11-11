a61186b8e7a9c2a80d675cbd53723cb5aaaa3b1c
Firrow <firrowmb@gmail.com>
2023-11-11 13:50:45 +0100

    Spell Bomb work

--- MagicDoom/Assets/Scripts/Spells/spellBomb.cs
+++ MagicDoom/Assets/Scripts/Spells/spellBomb.cs
@@ -5,17 +5,53 @@ using UnityEngine;
 public class spellBomb : MonoBehaviour
 {
     private Animator animator;
+    private float appearTime;
+    private bool explodeAnimation;
+    private int damage;
 
     void Start()
     {
         animator = this.GetComponent<Animator>();
-        //animator.SetBool("willExplode", true);
+        appearTime = Time.time;
+        explodeAnimation = false;
+        damage = 10;
     }
-
 
     void Update()
     {
+        if (!explodeAnimation)
+        {
+            if (Time.time - appearTime < 3)
+                return;
+            else
+            {
+                animator.SetBool("explode", true);
+                explodeAnimation = true;
+            }
+        }
 
+        RecalculateColliderShape();
     }
 
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.layer == LayerMask.NameToLayer("Enemies") && explodeAnimation == true)
+        {
+            collision.gameObject.GetComponent<Enemy>().TakeDamage(damage);
+        }
+    }
+
+    public void isExploded(string message)
+    {
+        if (message.Equals("exploded"))
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    private void RecalculateColliderShape()
+    {
+        Destroy(GetComponent<PolygonCollider2D>());
+        gameObject.AddComponent<PolygonCollider2D>();
+    }
 }
