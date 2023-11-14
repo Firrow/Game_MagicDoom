f597179e47fdc9f65b16d7506bdf5e099e7cfcdf
Firrow <firrowmb@gmail.com>
2023-11-14 17:33:57 +0100

    Setting up spell wave animation

--- MagicDoom/Assets/Scripts/Spells/spellWave.cs
+++ MagicDoom/Assets/Scripts/Spells/spellWave.cs
@@ -8,22 +8,25 @@ public class spellWave : MonoBehaviour
     private Vector3 movement;
     private Vector3 sensShoot = new Vector3(1, 0, 0);
     private int damage;
+    private Animator animator;
 
 
     void Start()
     {
         damage = 100;
         speed = 2.5f;
+
+        animator = this.GetComponent<Animator>();
     }
 
     void Update()
     {
         movement = new Vector3(sensShoot.x, 0, 0);
 
-        gameObject.transform.Translate(movement * speed * 5 * Time.deltaTime);
-        gameObject.transform.localScale += new Vector3(0.004f, 0.004f, 0);
+        gameObject.transform.Translate(movement * speed * 3 * Time.deltaTime);
+        gameObject.transform.localScale += new Vector3(0.003f, 0.003f, 0);
 
-        Destroy(this.gameObject, 2.0f);
+        RecalculateColliderShape();
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -32,6 +35,17 @@ public class spellWave : MonoBehaviour
         {
             collision.gameObject.GetComponent<Enemy>().TakeDamage(damage);
         }
+    }
+
+    private void RecalculateColliderShape()
+    {
+        Destroy(GetComponent<PolygonCollider2D>());
+        gameObject.AddComponent<PolygonCollider2D>();
+    }
+
+    private void DestroyWave()
+    {
+        Destroy(this.gameObject);
     }
 
 }
