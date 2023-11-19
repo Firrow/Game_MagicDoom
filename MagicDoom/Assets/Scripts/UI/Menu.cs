af98ce08bff50a63c738cf4847800c1bcb861a86
Firrow <firrowmb@gmail.com>
2023-11-19 19:23:57 +0100

    Update Menu.cs

--- MagicDoom/Assets/Scripts/UI/Menu.cs
+++ MagicDoom/Assets/Scripts/UI/Menu.cs
@@ -7,26 +7,6 @@ using UnityEngine.UI;
 
 public class Menu : MonoBehaviour
 {
-    /*private Button button;
-
-    void Start()
-    {
-        foreach (var button in GameObject.FindGameObjectsWithTag("Button"))
-        {
-            button.GetComponent<Button>().onClick.AddListener(ButtonSelected);
-        }
-    }
-
-    private void ButtonSelected()
-    {
-        Debug.Log("click");
-    }
-
-    private void OnDisable()
-    {
-        button.onClick.RemoveListener(ButtonSelected);
-    }*/
-
     public void LoadScreenDifficulty()
     {
         SceneManager.LoadScene("Difficulty");
