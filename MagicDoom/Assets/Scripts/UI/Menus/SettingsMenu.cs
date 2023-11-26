fbf1674d004f2c776012d73a70615a373132d958
Firrow <firrowmb@gmail.com>
2023-11-26 12:54:38 +0100

    Setting up Music/Sound sliders + file reorganization

---
+++ MagicDoom/Assets/Scripts/UI/Menus/SettingsMenu.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class SettingsMenu : MonoBehaviour
+{
+    public AudioMixer musicAudioMixer;
+    public AudioMixer soundAudioMixer;
+    public void SetVolumeMusic(float volume)
+    {
+        musicAudioMixer.SetFloat("musicVolume", volume);
+    }
+
+    public void SetVolumeSound(float volume)
+    {
+        soundAudioMixer.SetFloat("soundVolume", volume);
+    }
+}
