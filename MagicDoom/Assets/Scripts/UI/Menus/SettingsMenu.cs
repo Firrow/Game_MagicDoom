531051cfac192a5586caaad368d2756c3d86005e
Firrow <firrowmb@gmail.com>
2023-11-26 15:31:37 +0100

    Setting up list screen resolution

--- MagicDoom/Assets/Scripts/UI/Menus/SettingsMenu.cs
+++ MagicDoom/Assets/Scripts/UI/Menus/SettingsMenu.cs
@@ -2,11 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
+using TMPro;
+using UnityEngine.SceneManagement;
+using System.Linq;
 
 public class SettingsMenu : MonoBehaviour
 {
+    public Resolution[] resolutions;
+    public TMP_Dropdown resolutionDropdown;
     public AudioMixer musicAudioMixer;
     public AudioMixer soundAudioMixer;
+
+    private void Start()
+    {
+        AllResolutionSettings();
+    }
+
     public void SetVolumeMusic(float volume)
     {
         musicAudioMixer.SetFloat("musicVolume", volume);
@@ -20,5 +31,42 @@ public class SettingsMenu : MonoBehaviour
     public void SetFullscreen(bool isFullscreen)
     {
         Screen.fullScreen = isFullscreen;
+    }
+
+    public void ReturnMenu()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+
+    private void AllResolutionSettings()
+    {
+        //Get all resolutions in one occurence only + get the good one in starting game
+        resolutions = Screen.resolutions.Select(Resolution => new Resolution { width = Resolution.width, height = Resolution.height }).Distinct().ToArray();
+
+        resolutionDropdown.ClearOptions();
+        List<string> options = new List<string>();
+
+        int currentResolutionIndex = 0;
+        for (int i = 0; i < resolutions.Length; i++)
+        {
+            string option = resolutions[i].width + " x " + resolutions[i].height;
+
+            options.Add(option);
+
+            if (resolutions[i].width == Screen.currentResolution.width && resolutions[i].height == Screen.currentResolution.height)
+            {
+                currentResolutionIndex = i;
+            }
+        }
+
+        resolutionDropdown.AddOptions(options);
+        resolutionDropdown.value = currentResolutionIndex;
+        resolutionDropdown.RefreshShownValue();
+    }
+
+    public void SetResolution(int resolutionIndex)
+    {
+        Resolution resolution = resolutions[resolutionIndex];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
     }
 }
